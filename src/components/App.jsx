d4c64461a92d9e754cba625f21ad4879e70fcc69
Hruaia <hruaitluangasailo20@gmail.com>
2025-12-13 20:52:42 +0530

    Fix mobile view: PC-style scaling for home page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -32,166 +32,221 @@ function App() {
   return (
     <div className="app">
       <Routes>
-         <Route path="/forum" element={
+
+        {/* ===== FORUM ===== */}
+        <Route
+          path="/forum"
+          element={
             <div className="page-content">
-            <Navbar />
-            <ThreadListPage />
-            <Footer />
+              <Navbar />
+              <ThreadListPage />
+              <Footer />
+            </div>
+          }
+        />
+
+        <Route
+          path="/forum/:threadId"
+          element={
+            <div className="page-content">
+              <Navbar />
+              <ThreadDetailPage />
+              <Footer />
+            </div>
+          }
+        />
+
+        {/* ===== HOME (PC STYLE ON MOBILE) ===== */}
+        <Route
+          path="/"
+          element={
+            <div className="page-content overflow-x-hidden">
+              <Navbar />
+
+              {/* SCALE ONLY HOME CONTENT */}
+              <div className="home-scale-wrapper mx-auto max-w-[1400px]">
+                <LandingHeader />
+                <MainGrid />
+                <LowerGrid />
+                <ExploreServices />
+                <Feedback />
               </div>
-                } />
-      <Route path="/forum/:threadId" element={
-    <div className="page-content">
-       <Navbar/>
-    <ThreadDetailPage />
-    <Footer/>
-  </div>
-} />
 
+              <Footer />
+            </div>
+          }
+        />
 
+        {/* ===== OTHER PAGES (UNCHANGED) ===== */}
+        <Route
+          path="/about"
+          element={
+            <div className="page-content">
+              <Navbar />
+              <About2 />
+              <About />
+              <Footer />
+            </div>
+          }
+        />
 
+        <Route
+          path="/vision"
+          element={
+            <div className="page-content">
+              <Navbar />
+              <Vision />
+              <Footer />
+            </div>
+          }
+        />
 
+        <Route
+          path="/mission"
+          element={
+            <div className="page-content">
+              <Navbar />
+              <Mission />
+              <Footer />
+            </div>
+          }
+        />
 
-        <Route path="/" element={
-          <div className="page-content">
-            <Navbar />
-            <LandingHeader />
-            <MainGrid />
-            <LowerGrid />
-            <ExploreServices />
-            <Feedback/>
-            <Footer />
-          </div>
-        } />
-        <Route path="/about" element={
-          <div className="page-content">
-            <Navbar />
-            <About2 />
-            <About />
-            <Footer />
-          </div>
-        } />
-        <Route path="/vision" element={
-          <div className="page-content">
-            <Navbar />
-            <Vision />
-            <Footer />
-          </div>
-        } />
-        <Route path="/mission" element={
-          <div className="page-content">
-            <Navbar />
-            <Mission />
-            <Footer />
-          </div>
-        } />
-        <Route path="/values" element={
-          <div className="page-content">
-            <Navbar />
-            <Values />
-            <Footer />
-          </div>
-        } />
-        <Route path="/why-shiksha" element={
-          <div className="page-content">
-            <Navbar />
-            <WhySiksha />
-            <Footer />
-          </div>
-        } />
-        <Route path="/contact" element={
-          <div className="page-content">
-            <Navbar />
-            <Contact />
-            <Footer />
-          </div>
-        } />
+        <Route
+          path="/values"
+          element={
+            <div className="page-content">
+              <Navbar />
+              <Values />
+              <Footer />
+            </div>
+          }
+        />
 
-        <Route path="/login" element={
-          <div className="page-content">
-            <Navbar />
-            <Login />
-            <Footer />
-          </div>
-        } />
-        <Route path="/signup" element={
-          <div className="page-content">
-            <Navbar />
-            <Signup />
-            <Footer />
-          </div>
-        } />
-        <Route path="/current-affairs" element={
-          <div className="page-content">
-            <Navbar />
-            <CurrentAffairs />
-            <Footer />
-          </div>
-        } />
-        <Route path="/current-affairs" element={
-          <div className="page-content">
-            <Navbar />
-            <CurrentAffairs />
-            <Footer />
-          </div>
-        } />
-        
-        <Route path="/courses" element={
-    <div className="page-content">
-      <Navbar />
-      <Courses />
-      <Footer />
-    </div>
-  } />
+        <Route
+          path="/why-shiksha"
+          element={
+            <div className="page-content">
+              <Navbar />
+              <WhySiksha />
+              <Footer />
+            </div>
+          }
+        />
 
-      <Route path="/upcoming" element={<Upcoming />} />
+        <Route
+          path="/contact"
+          element={
+            <div className="page-content">
+              <Navbar />
+              <Contact />
+              <Footer />
+            </div>
+          }
+        />
 
-      <Route path="/terms" element={
-          <div className="page-content">
-            <Navbar />
-            <TermsCondition />
-            <Footer />
-          </div>
-        } />
+        <Route
+          path="/login"
+          element={
+            <div className="page-content">
+              <Navbar />
+              <Login />
+              <Footer />
+            </div>
+          }
+        />
 
-        <Route path="/faq" element={
-          <div className="page-content">
-            <Navbar />
-            <Faq />
-            <Footer />
-          </div>
-        } />
+        <Route
+          path="/signup"
+          element={
+            <div className="page-content">
+              <Navbar />
+              <Signup />
+              <Footer />
+            </div>
+          }
+        />
 
-      <Route path="/insight" element={
-          <div className="page-content">
-            <Navbar />
-            <Insight />
-            <Footer />
-          </div>
-        } />
+        <Route
+          path="/current-affairs"
+          element={
+            <div className="page-content">
+              <Navbar />
+              <CurrentAffairs />
+              <Footer />
+            </div>
+          }
+        />
 
-      <Route path="/training" element={
-          <div className="page-content">
-            <Training />
-          </div>
-        } />
+        <Route
+          path="/courses"
+          element={
+            <div className="page-content">
+              <Navbar />
+              <Courses />
+              <Footer />
+            </div>
+          }
+        />
 
-        <Route path="/general-studies" element={
-          <div className="page-content">
-            <Navbar />
-            <GeneralStudies />
-            <Footer />
-          </div>
-        } />         
+        <Route path="/upcoming" element={<Upcoming />} />
 
-        <Route path="/counselling" element={<Counselling />} />
-        <Route path="/counselling" element={
-          <div className="page-content">
-            <Navbar />
-            <Counselling />
-            <Footer />
-          </div>
-        } />
-        
+        <Route
+          path="/terms"
+          element={
+            <div className="page-content">
+              <Navbar />
+              <TermsCondition />
+              <Footer />
+            </div>
+          }
+        />
+
+        <Route
+          path="/faq"
+          element={
+            <div className="page-content">
+              <Navbar />
+              <Faq />
+              <Footer />
+            </div>
+          }
+        />
+
+        <Route
+          path="/insight"
+          element={
+            <div className="page-content">
+              <Navbar />
+              <Insight />
+              <Footer />
+            </div>
+          }
+        />
+
+        <Route path="/training" element={<Training />} />
+
+        <Route
+          path="/general-studies"
+          element={
+            <div className="page-content">
+              <Navbar />
+              <GeneralStudies />
+              <Footer />
+            </div>
+          }
+        />
+
+        <Route
+          path="/counselling"
+          element={
+            <div className="page-content">
+              <Navbar />
+              <Counselling />
+              <Footer />
+            </div>
+          }
+        />
 
       </Routes>
     </div>
